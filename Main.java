d6546114c7d01077018a3603ec1a04bb1be5fbb3
Erlandas <54706862+ErlandasL@users.noreply.github.com>
2020-09-15 09:15:18 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package BrickGame;
+
+import java.awt.Component;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame obj = new JFrame();
+		GamePlay gamePlay = new GamePlay();
+		obj.setBounds(10,10,700,600);
+		obj.setTitle("Break out ball");
+		obj.setResizable(false);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gamePlay);
+		obj.setVisible(true);
+	}
+
+}
